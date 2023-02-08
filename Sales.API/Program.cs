40218827643df4c2251fff4d72fbcb9b395ca0e1
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2023-02-08 14:51:53 -0500

    Begin countries controller

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -21,4 +21,11 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+app.UseCors(x => x
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
+
 app.Run();
--- Sales.WEB/Program.cs
+++ Sales.WEB/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7201/") });
 
 await builder.Build().RunAsync();
