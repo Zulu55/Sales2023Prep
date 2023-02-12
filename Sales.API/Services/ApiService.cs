f650e51f2d2f74e4d672fcd7501d74de392f4a52
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2023-02-12 17:54:28 -0500

    Fill countries, states and cities from an external API

---
+++ Sales.API/Services/ApiService.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json;
+using Sales.Shared.Responses;
+
+namespace Sales.API.Services
+{
+    public class ApiService : IApiService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _urlBase;
+        private readonly string _tokenName;
+        private readonly string _tokenValue;
+
+        public ApiService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _urlBase = _configuration["CoutriesAPI:urlBase"]!;
+            _tokenName = _configuration["CoutriesAPI:tokenName"]!;
+            _tokenValue = _configuration["CoutriesAPI:tokenValue"]!;
+        }
+
+        public async Task<Response> GetListAsync<T>(string servicePrefix, string controller)
+        {
+            try
+            {
+                HttpClient client = new()
+                {
+                    BaseAddress = new Uri(_urlBase),
+                };
+
+                client.DefaultRequestHeaders.Add(_tokenName, _tokenValue);
+                string url = $"{servicePrefix}{controller}";
+                HttpResponseMessage response = await client.GetAsync(url);
+                string result = await response.Content.ReadAsStringAsync();
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = result,
+                    };
+                }
+
+                List<T> list = JsonConvert.DeserializeObject<List<T>>(result)!;
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = list
+                };
+            }
+            catch (Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+    }
+}
