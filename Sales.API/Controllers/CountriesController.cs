4e9b421c4ccaa30ef5132c5c8e824b22afd81533
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2023-02-20 15:34:20 -0500

    Filter on countries

--- Sales.API/Controllers/CountriesController.cs
+++ Sales.API/Controllers/CountriesController.cs
@@ -25,6 +25,12 @@ namespace Sales.API.Controllers
             var queryable = _context.Countries
                 .Include(x => x.States)
                 .AsQueryable();
+
+            if(!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
             return Ok(await queryable
                 .OrderBy(x => x.Name)
                 .Paginate(pagination)
@@ -35,6 +41,12 @@ namespace Sales.API.Controllers
         public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
         {
             var queryable = _context.Countries.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(pagination.Filter))
+            {
+                queryable = queryable.Where(x => x.Name.ToLower().Contains(pagination.Filter.ToLower()));
+            }
+
             double count = await queryable.CountAsync();
             double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
             return Ok(totalPages);
--- Sales.Shared/DTOs/PaginationDTO.cs
+++ Sales.Shared/DTOs/PaginationDTO.cs
@@ -7,5 +7,7 @@
         public int Page { get; set; } = 1;
         
         public int RecordsNumber { get; set; } = 10;
+
+        public string? Filter { get; set; }
     }
 }
