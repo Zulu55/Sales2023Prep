b285031cb2c37a39d1fac4a023bc53ba4733bc9a
Juan Carlos Zuluaga Cardona <jzuluaga55@hotmail.com>
2023-03-03 14:18:48 -0500

    Change password

--- Sales.API/Controllers/AccountsController.cs
+++ Sales.API/Controllers/AccountsController.cs
@@ -109,6 +109,32 @@ namespace Sales.API.Controllers
             return Ok(await _userHelper.GetUserAsync(email));
         }
 
+
+        [HttpPost("changePassword")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult> ChangePasswordAsync(ChangePasswordDTO model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userHelper.GetUserAsync(User.Identity!.Name!);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _userHelper.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors.FirstOrDefault().Description);
+            }
+
+            return NoContent();
+        }
+
+
         private TokenDTO BuildToken(User user)
         {
             var claims = new List<Claim>
