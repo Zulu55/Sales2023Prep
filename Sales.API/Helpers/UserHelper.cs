f032220a4917dec489a3d596fa6fc3a8f60bc7d8
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-05 09:43:42 -0500

    Resend confirmation email and recover password

--- Sales.API/Helpers/UserHelper.cs
+++ Sales.API/Helpers/UserHelper.cs
@@ -95,5 +95,15 @@ namespace Sales.API.Helpers
         {
             return await _userManager.ConfirmEmailAsync(user, token);
         }
+
+        public async Task<string> GeneratePasswordResetTokenAsync(User user)
+        {
+            return await _userManager.GeneratePasswordResetTokenAsync(user);
+        }
+
+        public async Task<IdentityResult> ResetPasswordAsync(User user, string token, string password)
+        {
+            return await _userManager.ResetPasswordAsync(user, token, password);
+        }
     }
 }
