a03b69e77ae37b528a67ec84fb789c7fb2339174
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-05 13:00:31 -0500

    Products begin

---
+++ Sales.Shared/Entities/ProductCategory.cs
@@ -0,0 +1,15 @@
+﻿namespace Sales.Shared.Entities
+{
+    public class ProductCategory
+    {
+        public int Id { get; set; }
+
+        public Product Product { get; set; } = null!;
+
+        public int ProductId { get; set; }
+
+        public Category Category { get; set; } = null!;
+
+        public int CategoryId { get; set; }
+    }
+}
