4ca11fa26195f4408c76de62d2756e17f5f3fbc6
Juan Carlos Zuluaga <jzuluaga55@gmail.com>
2023-03-07 19:26:14 -0500

    Add home with the product catalog

--- Sales.API/Controllers/ProductsController.cs
+++ Sales.API/Controllers/ProductsController.cs
@@ -24,6 +24,7 @@ namespace Sales.API.Controllers
         }
 
         [HttpGet]
+        [AllowAnonymous]
         public async Task<ActionResult> Get([FromQuery] PaginationDTO pagination)
         {
             var queryable = _context.Products
@@ -44,6 +45,7 @@ namespace Sales.API.Controllers
 
 
         [HttpGet("totalPages")]
+        [AllowAnonymous]
         public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
         {
             var queryable = _context.Products
@@ -60,6 +62,7 @@ namespace Sales.API.Controllers
         }
 
         [HttpGet("{id:int}")]
+        [AllowAnonymous]
         public async Task<IActionResult> GetAsync(int id)
         {
             var product = await _context.Products
