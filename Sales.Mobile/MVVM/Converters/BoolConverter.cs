bd8471de2f3b92e13aac360c3c25b7dae7d473e0
Juan Zuluaga <Juan.Zuluaga_EX@kdrp.com>
2023-03-28 09:44:59 -0500

    Converters

---
+++ Sales.Mobile/MVVM/Converters/BoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sales.Mobile.MVVM.Converters
+{
+	public class BoolConverter : IValueConverter
+	{
+		public BoolConverter()
+		{
+		}
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolValue = (bool)value;
+            if (boolValue)
+            {
+                return "Sí";
+            }
+            return "No";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var stringValue = value.ToString();
+            if (stringValue == "Sí")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
+
